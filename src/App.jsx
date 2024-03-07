7a97c1529bc2b10c9c42c4cdf55ba6d0aea04d3a
Falar <fabio.lr81@gmail.com>
2024-03-07 16:17:58 +0100

    bug fix on change when no date specifed

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ function App() {
                 console.log(e)
             }
         }else{
-            setExpensesFiltered(expenses);
+            makeAPICall('', { method: 'GET' });
         }
     }
 
