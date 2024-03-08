d3a24ffc77ffcb3e2cb8d038dab984712a76fede
Falar <fabio.lr81@gmail.com>
2024-03-08 12:36:52 +0100

    set localizzation datepicker

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import DateRangePicker from 'rsuite/DateRangePicker';
 import 'rsuite/DateRangePicker/styles/index.css';
+import { CustomProvider } from 'rsuite';
+import itIT from 'rsuite/locales/it_IT';
 
 const Toolbar = ({hf,hcdg,modal}) => {    
     return (
         <nav className="navbar mt-3 navbar bg-light">
             <div className="container-fluid">
-                <div className="form-floating">            
-                    <DateRangePicker placeholder="Filtra per data" size="lg" showOneCalendar onChange={hcdg}/>        
+                <div className="form-floating">    
+                <CustomProvider locale={itIT}>    
+                    <DateRangePicker className='shadow' placeholder="Filtra per data" size="lg" showOneCalendar onChange={hcdg}/>    
+                    </CustomProvider>        
                 </div>
                 <div className="form-floating">
                     <input id="searchinputField" type="text" placeholder="Categoria, desc., data" className="form-control shadow" onChange={hf}></input>
